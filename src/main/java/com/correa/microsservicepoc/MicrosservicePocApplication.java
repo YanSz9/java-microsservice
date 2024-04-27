803aae51e4cb90dd100f69cb84cd265604050ae8
YanSz9 <yansouza009@gmail.com>
2024-04-27 09:12:47 -0300

    chore: start project

---
+++ src/main/java/com/correa/microsservicepoc/MicrosservicePocApplication.java
@@ -0,0 +1,13 @@
+package com.correa.microsservicepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correa/microsservicepoc/MicrosservicePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correa.microsservicepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
